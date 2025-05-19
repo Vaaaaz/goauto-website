4994105ddf9fc63a5e750a0405e27a709ef32f5b
Vaaaaz <lvaz80635@gmail.com>
2025-05-18 23:25:29 -0300

    alterações feitas e SEO adicionado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,22 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "GO Auto",
-  description: "A melhor estética automotiva a domicílio!",
+  description: "Transforme seu carro sem sair de casa. A GO Auto oferece detalhamento automotivo completo a domicílio com qualidade premium, rapidez e total conveniência em Poços de Caldas.",
+  keywords: [
+    "estética automotiva",
+    "detalhamento automotivo",
+    "lavagem automotiva",
+    "polimento automotivo",
+    "estética automotiva a domicílio",
+    "GO Auto",
+    "lavagem a domicílio",
+    "carro limpo em casa",
+    "serviço automotivo premium",
+    "higienização interna de carros",
+    "cuidados automotivos em casa",
+    "Poços de Caldas",
+    "carro novo em casa"
+  ]
 };
 
 export default function RootLayout({
